8c195b18057229610450994d0407baa160d67910
Arturo Bernal <arturober@gmail.com>
2025-10-17 13:49:35 +0200

    Bug

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { QuestionService } from './question.service';
-import { QuestionController } from './question.controller';
-import { PropertyQuestion } from './entities/property-question.entity';
-import { Property } from '@mikro-orm/core';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Module } from '@nestjs/common';
+import { RealstateProperty } from 'src/property/entities/realstate_property.entity';
+import { PropertyQuestion } from './entities/property-question.entity';
+import { QuestionController } from './question.controller';
+import { QuestionService } from './question.service';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([PropertyQuestion, Property])],
+  imports: [MikroOrmModule.forFeature([PropertyQuestion, RealstateProperty])],
   controllers: [QuestionController],
   providers: [QuestionService],
   exports: [QuestionService],
